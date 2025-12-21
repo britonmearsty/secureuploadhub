c3c0446cd68f8769d5c7bdfb8a16c83696e6c9f2
Brito cheruyot <mearstybriton.com>
2025-12-21 19:17:32 +0330

    lili

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: { turbo: false },
   async rewrites() {
     return [
       {
